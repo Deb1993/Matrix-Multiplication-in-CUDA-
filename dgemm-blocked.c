6211661d25d024d38b7f8b25e79482915084e7fb
Kunal Kashilkar <gauravkashilkar@gmail.com>
2018-01-23 00:23:50 -0800

    vectorization added

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -6,8 +6,9 @@
  *    Support CBLAS interface
  */
 
+#include <math.h>
+#include <stdio.h>
 #include <emmintrin.h>
-
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define L1_CACHE 32*1024
@@ -20,21 +21,45 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
-#define TRANSPOSE true
-
 int calculate_block_size(int n, int cache_size) {
     double num_blocks = n*sqrt(3/(1.0*cache_size));
     return num_blocks >= 1 ? sqrt(cache_size/3) : n;
 }
 
+static void do_vectorize (int lda, int ii, int jj, int kk, double *restrict A, double *restrict B, double *restrict C) {
+
+        __m128d c00_c01 = _mm_loadu_pd (C + ii*lda + jj);
+        __m128d c10_c11 = _mm_loadu_pd(C + (ii + 1)*lda + jj);
+        for(int i = 0; i < 2; i+=1)  {
+            __m128d a1 = _mm_load1_pd(A + ii*lda + kk + i);
+            __m128d a2 = _mm_load1_pd(A + (ii + 1)*lda + kk + i);
+
+            __m128d b = _mm_loadu_pd(B + (kk + i)*lda + jj);
+
+            c00_c01 = _mm_add_pd(c00_c01, _mm_mul_pd(a1, b));
+            c10_c11 = _mm_add_pd(c10_c11, _mm_mul_pd(a2, b));
+        }
+
+        _mm_storeu_pd(C + ii*lda + jj, c00_c01);
+        _mm_storeu_pd(C + (ii + 1)*lda + jj, c10_c11);
+}
+
+
+static void do_block_vectorized (int lda, int M, int N, int K, double *restrict A, double *restrict B, double *restrict C)
+{
+    for (int i = 0; i < M; i+=2) {
+        for (int j = 0; j < N; j+=2) {
+            for (int k = 0; k < K; k+=2){
+                do_vectorize(lda, i, j, k, A, B, C);
+            }
+        }
+    }
+}
+
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-<<<<<<< 547874f4a955f2da7a290bc5cc307cff82cfaaaf
-void do_block (int lda, int M, int N, int K, double* restrict A, double* restrict B, double* restrict C)
-=======
 static void do_block (int lda, int M, int N, int K, double *restrict A, double *restrict B, double *restrict C)
->>>>>>> restrict keyword used; TRANSPOSE define
 {
     /* For each row i of A */
     for (int i = 0; i < M; ++i)
@@ -55,41 +80,41 @@ static void do_block (int lda, int M, int N, int K, double *restrict A, double *
 
 void do_vector (int lda, double* restrict A, double* restrict B, double* restrict C)
 {
-			register __m128d c00_c01 = _mm_loadu_pd(C);
-			register __m128d c10_c11 = _mm_loadu_pd(C + lda);
-			register __m128d c20_c21 = _mm_loadu_pd(C + 2*lda);
-			register __m128d c30_c31 = _mm_loadu_pd(C + 3*lda);
-                        register __m128d c02_c03 = _mm_loadu_pd(C + 2);
-                        register __m128d c12_c13 = _mm_loadu_pd(C + lda + 2);
-                        register __m128d c22_c23 = _mm_loadu_pd(C + 2*lda + 2);
-                        register __m128d c32_c33 = _mm_loadu_pd(C + 3*lda + 2);
+    register __m128d c00_c01 = _mm_loadu_pd(C);
+    register __m128d c10_c11 = _mm_loadu_pd(C + lda);
+    register __m128d c20_c21 = _mm_loadu_pd(C + 2*lda);
+    register __m128d c30_c31 = _mm_loadu_pd(C + 3*lda);
+    register __m128d c02_c03 = _mm_loadu_pd(C + 2);
+    register __m128d c12_c13 = _mm_loadu_pd(C + lda + 2);
+    register __m128d c22_c23 = _mm_loadu_pd(C + 2*lda + 2);
+    register __m128d c32_c33 = _mm_loadu_pd(C + 3*lda + 2);
 
-				for (int i = 0; i < 4; i++)
-					{
-						register __m128d a1 = _mm_load1_pd(A + i);  
-						register __m128d a2 = _mm_load1_pd(A + i + lda);
-						register __m128d a3 = _mm_load1_pd(A + i + 2*lda);
-						register __m128d a4 = _mm_load1_pd(A + i + 3*lda);
-						register __m128d b  = _mm_loadu_pd(B + i*lda);
-						//register __m128d a4 = _mm_load1_pd(A + i + 3*lda);
-						register __m128d b1 = _mm_loadu_pd(B + i*lda + 2);
-						c00_c01 = _mm_add_pd(c00_c01, _mm_mul_pd(a1,b));
-						c10_c11 = _mm_add_pd(c10_c11, _mm_mul_pd(a2,b));
-						c20_c21 = _mm_add_pd(c20_c21, _mm_mul_pd(a3,b));
-						c30_c31 = _mm_add_pd(c30_c31, _mm_mul_pd(a4,b));
-                                                c02_c03 = _mm_add_pd(c02_c03, _mm_mul_pd(a1,b1));
-                                                c12_c13 = _mm_add_pd(c12_c13, _mm_mul_pd(a2,b1));
-                                                c22_c23 = _mm_add_pd(c22_c23, _mm_mul_pd(a3,b1));
-                                                c32_c33 = _mm_add_pd(c32_c33, _mm_mul_pd(a4,b1));
-				}
-_mm_storeu_pd(C, c00_c01);
-_mm_storeu_pd(C + lda, c10_c11);
-_mm_storeu_pd(C + 2*lda, c20_c21);
-_mm_storeu_pd(C + 3*lda, c30_c31);
-_mm_storeu_pd(C + 2, c02_c03);
-_mm_storeu_pd(C + lda + 2, c12_c13);
-_mm_storeu_pd(C + 2*lda + 2, c22_c23);
-_mm_storeu_pd(C + 3*lda + 2, c32_c33);
+    for (int i = 0; i < 4; i++)
+    {
+        register __m128d a1 = _mm_load1_pd(A + i);  
+        register __m128d a2 = _mm_load1_pd(A + i + lda);
+        register __m128d a3 = _mm_load1_pd(A + i + 2*lda);
+        register __m128d a4 = _mm_load1_pd(A + i + 3*lda);
+        register __m128d b  = _mm_loadu_pd(B + i*lda);
+        //register __m128d a4 = _mm_load1_pd(A + i + 3*lda);
+        register __m128d b1 = _mm_loadu_pd(B + i*lda + 2);
+        c00_c01 = _mm_add_pd(c00_c01, _mm_mul_pd(a1,b));
+        c10_c11 = _mm_add_pd(c10_c11, _mm_mul_pd(a2,b));
+        c20_c21 = _mm_add_pd(c20_c21, _mm_mul_pd(a3,b));
+        c30_c31 = _mm_add_pd(c30_c31, _mm_mul_pd(a4,b));
+        c02_c03 = _mm_add_pd(c02_c03, _mm_mul_pd(a1,b1));
+        c12_c13 = _mm_add_pd(c12_c13, _mm_mul_pd(a2,b1));
+        c22_c23 = _mm_add_pd(c22_c23, _mm_mul_pd(a3,b1));
+        c32_c33 = _mm_add_pd(c32_c33, _mm_mul_pd(a4,b1));
+    }
+    _mm_storeu_pd(C, c00_c01);
+    _mm_storeu_pd(C + lda, c10_c11);
+    _mm_storeu_pd(C + 2*lda, c20_c21);
+    _mm_storeu_pd(C + 3*lda, c30_c31);
+    _mm_storeu_pd(C + 2, c02_c03);
+    _mm_storeu_pd(C + lda + 2, c12_c13);
+    _mm_storeu_pd(C + 2*lda + 2, c22_c23);
+    _mm_storeu_pd(C + 3*lda + 2, c32_c33);
 
 }
 
@@ -105,11 +130,7 @@ void do_block_vector (int lda, int M, int N, int K, double* restrict A, double* 
 }
 }
 
-<<<<<<< 547874f4a955f2da7a290bc5cc307cff82cfaaaf
-void do_block1 (int lda, int M, int N, int K, double* restrict A, double* restrict B, double* restrict C)
-=======
 void do_block1 (int lda, int M, int N, int K, double *restrict A, double *restrict B, double *restrict C)
->>>>>>> restrict keyword used; TRANSPOSE define
 {
 
     int block_size_2 = calculate_block_size(lda, L2_CACHE);
@@ -126,22 +147,21 @@ void do_block1 (int lda, int M, int N, int K, double *restrict A, double *restri
                 int N_1 = min (block_size, N-j);
                 int K_1 = min (block_size, K-k);
 
+                if(M_1&1 == 0 && N_1&&1 == 0 && K_1&1 == 0) {
+                    do_block_vectorized(lda, M_1, N_1, K_1, A + i*lda + k, B + k*lda + j, C + i*lda + j);
+                } else {
                 /* Perform individual block dgemm */
 		if((M_1 % 4) == 0 && (N_1 % 4) == 0 && (K_1 % 4) == 0)
 			do_block_vector(lda,M_1,N_1,K_1, A + i*lda + k, B + k*lda + j, C + i*lda + j);
 		else
                 	do_block(lda, M_1, N_1, K_1, A + i*lda + k, B + k*lda + j, C + i*lda + j);
-            }
+        }
 }
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in row-major order
  * On exit, A and B maintain their input values. */  
-<<<<<<< 547874f4a955f2da7a290bc5cc307cff82cfaaaf
-void square_dgemm (int lda, double* restrict A, double* restrict B, double* restrict C)
-=======
 void square_dgemm (int lda, double *restrict A, double *restrict B, double *restrict C)
->>>>>>> restrict keyword used; TRANSPOSE define
 {
     int block_size_2 = calculate_block_size(lda, L2_CACHE);
     /* For each block-row of A */ 
